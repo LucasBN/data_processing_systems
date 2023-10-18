c6b37a5be2d7b31a292d9c926769b3c3991b3b7e
Lucas Graeff Buhl-Nielsen <lucas.graeff-buhl-nielsen21@imperial.ac.uk>
2023-10-18 13:19:25 +0100

    project constructor

--- main.cpp
+++ main.cpp
@@ -1,96 +1,86 @@
 #include <iostream>
+#include <set>
 #include <string>
 #include <tuple>
-#include <set>
 
 using namespace std;
 
-template <size_t I = 0, typename... Tp>
-void printTuple(std::tuple<Tp...> &t)
-{
-  std::cout << std::get<I>(t) << "\t";
-  // do things
-  if constexpr (I + 1 != sizeof...(Tp))
-    printTuple<I + 1>(t);
+/* ------------------------------------------------ */
+/* Tuple Helper Functions                           */
+/* ------------------------------------------------ */
+template <unsigned... s>
+struct seq {
+  typedef seq<s...> type;
+};
+template <unsigned max, unsigned... s>
+struct make_seq : make_seq<max - 1, max - 1, s...> {};
+template <unsigned... s>
+struct make_seq<0, s...> : seq<s...> {};
+
+template <unsigned... s, typename Tuple>
+auto extract_tuple(seq<s...>, Tuple& tup) {
+  return make_tuple(get<s>(tup)...);
 }
+/* ------------------------------------------------ */
 
 template <typename... types>
-struct Relation
-{
+struct Relation {
   using OutputType = tuple<types...>;
   array<string, sizeof...(types)> schema;
   set<OutputType> data;
+
   Relation(){};
-  Relation(array<string, sizeof...(types)> schema, set<OutputType> data) : schema(schema), data(data){};
-
-  void printRelation()
-  {
-    cout << "";
-
-    for (string label : schema)
-      cout << label << "\t";
-
-    cout << "\n";
-
-    for (OutputType row : data)
-    {
-      printTuple(row);
-      cout << "\n";
-    }
-  }
+  Relation(array<string, sizeof...(types)> schema)
+      : schema(schema), data(set<OutputType>()){};
+  Relation(array<string, sizeof...(types)> schema, set<OutputType> data)
+      : schema(schema), data(data){};
 };
 
 template <typename... types>
-struct Operator : public Relation<types...>
-{
+struct Operator : public Relation<types...> {
+  Operator(){};
+  Operator(array<string, sizeof...(types)> schema)
+      : Relation<types...>(schema){};
 };
 
 template <typename InputOperator, typename... types>
-struct Project : public Operator<types...>
-{
+struct Project : public Operator<types...> {
+  using OutputType = tuple<types...>;
+
   InputOperator input;
   set<pair<string, string>> projections;
 
-  Project(InputOperator input, set<pair<string, string>> projections) : input(input), projections(projections)
-  {
+  Project(InputOperator input, set<pair<string, string>> projections)
+      : input(input), projections(projections) {
+    array<string, sizeof...(types)> schema = {};
+    set<OutputType> data;
 
-    array<string, sizeof...(types)> new_schema = {};
-    int counter = 0;
-    for (string label : this->schema)
-    {
-      for (pair<string, string> projection : projections)
-      {
-        if (strcmp(projection.first.c_str(), label.c_str()) != 0)
-        {
-          new_schema[counter] = projection.second;
-          counter++;
-        }
-      }
+    /* Define new schema based on given projection and keep the
+       corresponding columns. */
+    int i = 0;
+    for (pair<string, string> projection : projections) {
+      schema[i] = projection.second;
     }
-    this->schema = new_schema;
-    this->data = {
-        make_tuple("lucas"),
-        make_tuple("holger"),
-    };
+
+    /* Set parent struct attributes according to projection. */
+    this->schema = schema;
+    this->data = data;
   };
 };
 
-int main()
-{
+int main() {
+  Relation<int, string> users({"id", "name"}, {
+                                                  make_tuple(1, "lucas"),
+                                                  make_tuple(2, "holger"),
+                                              });
 
-  Relation<int, string> users(
-      {"id", "name"},
-      {
-          make_tuple(1, "lucas"),
-          make_tuple(2, "holger"),
-      });
+  Relation<string> usernames =
+      Project<decltype(users), string>(users, {{"name", "username"}});
 
-  Relation<string> usernames = Project<decltype(users), string>(users, {{"name", "username"}});
-
-  users.printRelation();
-  cout << "\n";
-  usernames.printRelation();
-  cout << "\n";
+  tuple<int, string, int> my_tup = make_tuple(1, "lucas", 52);
+  int i = 2;
+  auto skip_2nd = extract_tuple(seq<0, 2>(), my_tup);
+  cout << get<1>(skip_2nd) << "\n";
 
   return 0;
 }
